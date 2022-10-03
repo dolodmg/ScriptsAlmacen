91ed29c6990fb8d7f52a038f8e1e71c54b9c74c2
dolodmg <65740837+dolodmg@users.noreply.github.com>
2022-10-03 18:44:20 -0300

    Update Triggers Dolores Dominguez.sql

--- Triggers Dolores Dominguez.sql
+++ Triggers Dolores Dominguez.sql
@@ -31,3 +31,27 @@ VALUES
     CONCAT('Se agregó el empleado ',NEW.nombreApellido,' con id: ',NEW.idEmpleado, ' al sistema'),
     USER(),
     CURRENT_DATE());
+    
+-- Trigger que se dispara cuando se agrega un itemVenta, y actualiza el precio en la tabla Venta para el idVenta
+-- que corresponde a ese item que se agrega
+CREATE TRIGGER tr_actualizar_monto_venta
+AFTER INSERT ON ItemVentas 
+FOR EACH ROW
+UPDATE Ventas VA SET precioTotal = (SELECT precio
+FROM (
+SELECT SUM(IV.cantidad*IV.precioUnidad) AS precio
+FROM Ventas V, ItemVentas IV
+WHERE V.idVenta = IV.idVenta AND V.idVenta = VA.idVenta
+GROUP BY V.idVenta) AS preciotot);
+
+-- Trigger que se dispara cuando se agrega un itemCompra, y actualiza el precio en la tabla Compra para el idCompra
+-- que corresponde a ese item que se agrega
+CREATE TRIGGER tr_actualizar_monto_compra
+AFTER INSERT ON ItemCompras
+FOR EACH ROW
+UPDATE Compras CA SET montoTotal = (SELECT precio
+FROM (
+SELECT SUM(IC.cantidad*IC.precioUnidad) AS precio
+FROM Compras C, ItemCompras IC
+WHERE C.idCompra = IC.idCompra AND C.idCompra = CA.idCompra
+GROUP BY C.idCompra) AS preciotot);
