9fef00ad4fb10b0e1c83a0b9fdf54c994b1fd3dc
dolodmg <65740837+dolodmg@users.noreply.github.com>
2022-10-22 18:58:35 -0300

    Add files via upload

---
+++ DCL Dolores Dominguez.sql
@@ -0,0 +1,10 @@
+-- Se crean los dos usuarios y sus contraseñas
+CREATE USER 'dolores'@'localhost' IDENTIFIED BY '12345';
+CREATE USER 'administrador1'@'localhost' IDENTIFIED BY 'abcdef';
+
+-- Asigna permisos de lectura al usuario administrador1
+GRANT SELECT ON *.* TO 'administrador1'@'localhost';
+
+-- Asigna permisos de lectura, inserción y modificación al usuario dolores
+GRANT SELECT, INSERT, UPDATE ON *.* TO 'dolores'@'localhost';
+
